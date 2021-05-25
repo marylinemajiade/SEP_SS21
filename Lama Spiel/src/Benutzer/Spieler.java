39ae5626c56179880e48232d65660d6cfeaa3768
Hamza Barinae <bariane@rhrk.uni.kl.de>
2021-05-25 11:20:09 +0200

    Konto Klasse erstellen, die verschieden Methode definieren. Spieler Klasse erstellen, Methoden definieren. biede Klassen noch nocht fertig!

--- Lama Spiel/src/Benutzer/Spieler.java
+++ Lama Spiel/src/Benutzer/Spieler.java
@@ -1,6 +1,38 @@
 package Benutzer;
 
-public class Spieler {
+/**
+ * This Class defines all the methods, that a player needs during the game.
+ *
+ * @author Hamza Bariane
+ *
+ */
 
+public class Spieler extends Konto{
+
+    public Spieler(String benutzername, String passwort) {
+        super(benutzername, passwort);
+    }
+
+    void erstelleKonto(String benutzername ,String passwort){
+
+    }
+
+    void löscheKonto(String benutzername, String passwort){
+
+    }
+
+    void spielraumErstellen(int anzahlSpieler){
+
+    }
+
+    void botHinzufügen(){} // wir brauchen dazu ein Typparameter (botType Klasse Bot)
+
+    void botEntfernen(){}// wir brauchen dazu ein Typparameter (botType Klasse Bot)
+
+    void spieleraumBetreten(){}
+
+    void spileraumVerlassen(){}
+
+    void chatBenutzen(){}
 
 }
