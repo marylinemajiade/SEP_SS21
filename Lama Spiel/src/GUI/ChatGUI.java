238a6066f9a59c622692f32b4f46edbc5abc0cec
Catharina Helten <helten@rhrk.uni-kl.de>
2021-05-28 16:13:39 +0200

    Chat-GUI Version2

--- Lama Spiel/src/GUI/ChatGUI.java
+++ Lama Spiel/src/GUI/ChatGUI.java
@@ -1,40 +1,82 @@
 package GUI;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 public class ChatGUI extends Application{
 
-    Button button1;
+    // Input-Textfeld zum Schreiben
+    private TextField inputArea = new TextField();
+    // Output-Textfeld zum Anzeigen der Nachrichten
+    private TextArea outputArea = new TextArea();
+
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-    primaryStage.setTitle("LAMA Chat");
+    public void start(Stage stage) throws Exception {
 
 
-    Button button1 = new Button("senden");
-
-
-
-    StackPane layout = new StackPane();
-
-    layout.getChildren().add(button1);
+        // Header-Text
+        Label headerText = new Label("Chatte hier mit anderen LAMA-Spielern!");
+        // weiterer Text, der angezeigt werden soll
+        Label input = new Label("Nachricht: ");
 
 
 
+        Button sendenButton = new Button("Senden");
 
-    Scene scene = new Scene(layout, 300,250);
-    primaryStage.setScene(scene);
-    primaryStage.show();
+        //wenn man auf Senden drückt, soll der Text genommen werden und mit der writeOutput-Methode ausgegeben
+        sendenButton.setOnAction(new EventHandler()
+        {
+            @Override
+            public void handle(Event event) {
+                writeOutput(inputArea.getText());
+            }
+        });
 
+        //erzeugt die Fläche mit Größe und Style
+        BorderPane borderpane = new BorderPane();
+
+        borderpane.setPrefSize(400, 400);
+
+        borderpane.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+
+
+
+        // was soll wo auf der Fläche angezeigt werden
+        borderpane.setRight(sendenButton);
+        borderpane.setTop(headerText);
+        borderpane.setLeft(input);
+        borderpane.setBottom(outputArea);
+        borderpane.setCenter(inputArea);
+        BorderPane.setAlignment(headerText,Pos.BOTTOM_CENTER);
+        BorderPane.setAlignment(input,Pos.TOP_LEFT);
+
+        // stage = Fenster, scene = Inhalt auf Fenster
+        Scene scene = new Scene(borderpane);
+
+        stage.setScene(scene);
+
+        stage.setTitle("LAMA Chat");
+
+        stage.show();
+
+
+    }
+
+    private void writeOutput(String msg)
+    {
+        // hier muss später noch die richtige Spieler-ID eingefügt werden
+        this.outputArea.appendText("Spieler " + "1" + ": " + msg + "\n");
     }
 
     public static void main (String[] args){
