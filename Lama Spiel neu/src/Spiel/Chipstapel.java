6e32a98964766166d31caa44d0981813c20356e3
Vanessa Stein <steinv@rhrk.uni-kl.de>
2021-06-03 19:44:47 +0200

    Package Spiel mit Klassen Chipstapel und Spielrunde erstellt

---
+++ Lama Spiel neu/src/Spiel/Chipstapel.java
@@ -0,0 +1,24 @@
+package Spiel;
+
+public class Chipstapel {
+
+    public Integer getWeiss(){
+        return null;
+    }
+
+    public Integer getSchwarz(){
+        return null;
+    }
+
+    public Integer getPunkte(){
+        return null;
+    }
+
+    public void setWeiss(int anzahlchips){
+
+    }
+
+    public void setSchwarz(int anzahlchips){
+
+    }
+}
---
+++ Lama Spiel neu/src/Spiel/Spielrunde.java
@@ -0,0 +1,63 @@
+package Spiel;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class Spielrunde {
+
+    int spielraumId;
+
+    public Integer getRaumId(){
+        return null;
+    }
+
+    public void karteAblegen(String benutzername, int karte){
+
+    }
+
+    public Integer karteZiehen(String benutzername){
+        return null;
+    }
+
+    public void aussteigen(String benutzername){
+
+    }
+
+    public void chipsTauschen(boolean zehngegeneins, String benutzername){
+
+    }
+
+    public void chipAbgeben(boolean zehnerchip, String benutzername){
+
+    }
+
+    public String anDerReihe(){
+        return null;
+    }
+
+    public void spielStarten(){
+
+    }
+
+    public HashSet getHandkarten(String benutzername){
+        return null;
+    }
+
+    public Stack getAblagestapel(){
+        return null;
+    }
+
+    public Stack getNachziehstapel(){
+        return null;
+    }
+
+    public Chipstapel getChipstapel(String benutzername){
+        return null;
+    }
+
+    public void spielraumVerlassen(String benutzername){
+
+    }
+
+
+}
