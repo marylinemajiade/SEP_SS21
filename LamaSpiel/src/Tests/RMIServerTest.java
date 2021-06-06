70892e8efcaf44ec92f59bde2716b816ea5b4762
Nick Jochum <njochum@rhrk.uni-kl.de>
2021-06-06 18:42:04 +0200

    Getter-Methoden zu RMI-Server hinzugefügt

--- LamaSpiel/src/Tests/RMIServerTest.java
+++ LamaSpiel/src/Tests/RMIServerTest.java
@@ -1,15 +1,87 @@
 package Tests;
 
+import RMI.RMIServer;
 import org.junit.Assert;
-import RMI.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-public class RMIServerTest {
+class RMIServerTest {
 
-    @Test
-    public void Test1(){    //Testet Methode
-        RMIServer rmiserver;
-
+    @org.junit.jupiter.api.Test
+    void registriereClient() {
+        RMIServer rmiserver = new RMIServer();
         Assert.assertEquals(3,3);
+    }
+
+    @org.junit.jupiter.api.Test
+    void benutzerdatenPruefen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void benutzerRegistrieren() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void benutzerLoeschen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendeChatnachricht() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spielraumBeitreten() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spielraumVerlassen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void botHinzufuegen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void botEntfernen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spielStarten() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void chipsTauschen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void chipAbgeben() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void karteAblegen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void karteZiehen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void aussteigen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBestenliste() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLobby() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSpielrunden() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBenutzerdaten() {
     }
 }
