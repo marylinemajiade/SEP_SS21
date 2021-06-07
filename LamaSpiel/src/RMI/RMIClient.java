96338d010581d701505913d0116b79bbff8e7a5e
Hamza Barinae <bariane@rhrk.uni.kl.de>
2021-06-07 11:21:13 +0200

    RMIClientIF, RMIClient (Methoden mit Exceptions). Änderung auf Benutzerdaten. (noch nicht fertig)

--- LamaSpiel/src/RMI/RMIClient.java
+++ LamaSpiel/src/RMI/RMIClient.java
@@ -1,4 +1,44 @@
 package RMI;
 
-public class RMIClient implements RMIClientIF {
+import Highscore.Bestenliste;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIClient extends UnicastRemoteObject implements RMIClientIF {
+
+
+    public RMIClient() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public void aktualisiereSpielstatus(Spielraum spielraum) throws RemoteException {
+
+    }
+
+    @Override
+    public void uebertrageChatnachricht(String benutzername, String nachricht) throws RemoteException {
+
+    }
+
+    @Override
+    public String getBenutzername() throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public void akutalisiereBestenliste(Bestenliste bestenliste) throws RemoteException {
+
+    }
+
+    @Override
+    public void setSpielraum() throws RemoteException {
+
+    }
+
+    @Override
+    public boolean isBot() throws RemoteException {
+        return false;
+    }
 }
--- LamaSpiel/src/Tests/BestenlisteTest.java
+++ LamaSpiel/src/Tests/BestenlisteTest.java
@@ -1,144 +1,115 @@
 package Tests;
 
-import Highscore.Bestenliste;
+import java.lang.Object.javafx.util.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
-
+/**
+ * Die Klasse BestenlisteTest dient zum Testen der Klasse Klasse Bestenliste mit JUnitTests
+ *
+ * @author Vanessa Stein
+ */
 
 class BestenlisteTest {
 
-    public HashMap<String,Integer> liste;
+    private HashMap liste;
 
+    /**
+     * Initialisiert eine leere Hashmap vor jeder Testmethode
+     */
     @BeforeEach
     void setup() {
-        Bestenliste bestenliste = new Bestenliste();
-        this.liste = Bestenliste.bestenliste;
+        this.liste = new HashMap<String, Integer>();
     }
 
+    /**
+     * Testet die Methode eintragHinzufuegen()
+     */
     @Test
-    void eintragHinzufuegen(String spieler, boolean isbot) {
+    void eintragHinzufuegen() {
         assertTrue(liste.isEmpty());
         assertEquals(0, liste.size());
 
-        Bestenliste.eintragHinzufuegen("Spieler1", false);
+        liste.put("Spieler1", 5);
         assertFalse(liste.isEmpty());
-        assertEquals(1,liste.size());
+        assertTrue(liste.size() == 1);
 
-        Bestenliste.eintragHinzufuegen("Spieler2", false);
-        assertEquals(2, liste.size() );
+        liste.put("Spieler2", 7);
+        assertTrue(liste.size() == 2);
 
         assertTrue(liste.containsKey("Spieler1"));
-        assertEquals(1, liste.get("Spieler1"));
+        assertEquals(5, liste.get("Spieler1"));
         assertTrue(liste.containsKey("Spieler2"));
-        assertEquals(1, liste.get("Spieler2"));
+        assertEquals(7, liste.get("Spieler2"));
 
-        Bestenliste.eintragHinzufuegen("Spieler2", false);
+        liste.put("Spieler2", 8);
         assertTrue(liste.containsKey("Spieler2"));
-        assertEquals(2, liste.get("Spieler2"));
-
-        Bestenliste.eintragHinzufuegen("Bot1", true);
-        assertFalse(liste.containsKey("Bot1"));
-
+        assertEquals(8, liste.get("Spieler2"));
     }
 
-
+    /**
+     * Testet die Methode getScore()
+     */
     @Test
-    void getScore(String spieler) {
+    void getScore() {
         assertTrue(liste.isEmpty());
+        liste.put("Spieler1", 5);
+        assertEquals(5, liste.get("Spieler1"));
 
-        Bestenliste.eintragHinzufuegen("Spieler1", false);
-        Bestenliste.eintragHinzufuegen("Spieler1", false);
-        Bestenliste.eintragHinzufuegen("Spieler1", false);
-        assertEquals(3, liste.get("Spieler1"));
-
-        assertThrows(NoSuchElementException.class, () -> liste.get("Spieler2"));
+        Assertions.assertThrows(NoSuchElementException.class, () -> liste.get("Spieler2"));
     }
 
     @Test
     void getTopZehn() {
-        assertTrue(liste.isEmpty());
+        ArrayList<Pair<String, Integer>> entries = new ArrayList<>();
 
-        for (int i = 1; i <= 11; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler1", false);
-        }
-
-        for (int i = 1; i <= 10; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler2", false);
-        }
-
-        for (int i = 1; i <= 9; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler3", false);
-        }
-
-        for (int i = 1; i <= 8; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler4", false);
-        }
-
-        for (int i = 1; i <= 7; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler5", false);
-        }
-
-        for (int i = 1; i <= 6; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler6", false);
-        }
-
-        for (int i = 1; i <= 5; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler7", false);
-        }
-
-        for (int i = 1; i <= 4; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler8", false);
-        }
-
-        for (int i = 1; i <= 3; i++) {
-            Bestenliste.eintragHinzufuegen("Spieler9", false);
-        }
-
-        Bestenliste.eintragHinzufuegen("Spieler10", false);
-        Bestenliste.eintragHinzufuegen("Spieler10", false);
-
-        Bestenliste.eintragHinzufuegen("Spieler11", false);
+        liste.put("Spieler1", 5);
+        liste.put("Spieler2", 7);
+        liste.put("Spieler3", 1);
+        liste.put("Spieler4", 3);
+        /*
+        noch nicht fertig
+         */
 
 
-        ArrayList topten = Bestenliste.getTopZehn();
-
-        assertEquals(10, topten.size());
-        assertEquals("Spieler1=11", topten.get(0).toString());
-        assertEquals("Spieler2=10", topten.get(1).toString());
-        assertEquals("Spieler3=9", topten.get(2).toString());
-        assertEquals("Spieler4=8", topten.get(3).toString());
-        assertEquals("Spieler5=7", topten.get(4).toString());
-        assertEquals("Spieler6=6", topten.get(5).toString());
-        assertEquals("Spieler7=5", topten.get(6).toString());
-        assertEquals("Spieler8=4", topten.get(7).toString());
-        assertEquals("Spieler9=2", topten.get(8).toString());
-        assertEquals("Spieler10=1", topten.get(9).toString());
 
     }
 
-
+    /**
+     * Testet die Methode eintragloeschen()
+     */
     @Test
-    void eintragLoeschen(String spieler) {
-
+    void eintragLoeschen() {
+        /*
+        Stellt sicher, dass liste.size() nicht < 0 wird
+         */
         assertTrue(liste.isEmpty());
         assertEquals(0, liste.size());
-
-        Bestenliste.eintragHinzufuegen("Spieler1", false);
-        Bestenliste.eintragHinzufuegen("Spieler2", false);
+        liste.remove("Spieler1");
+        assertEquals(0, liste.size());
+        /*
+        Stellt sicher, dass Eintraege für anschließende Tests ordentlich aufgenommen wurden
+         */
+        liste.put("Spieler1", 5);
+        liste.put("Spieler2", 7);
         assertEquals(2, liste.size());
-
-        Bestenliste.eintragLoeschen("Spieler2");
+        /*
+        Testet, ob liste.size() nach Entfernen eines Eintrags um 1 dekrementiert wird
+         */
+        liste.remove("Spieler2");
         assertEquals(1, liste.size());
-
+        /*
+        Testet, ob Key nach Loeschen wirklich nicht mehr enthalten ist
+         */
         assertFalse(liste.containsKey("Spieler2"));
-        assertThrows(NoSuchElementException.class, () -> Bestenliste.eintragLoeschen("Spieler2"));
+        Assertions.assertThrows(NoSuchElementException.class, () -> liste.get("Spieler2"));
+
 
     }
 }
