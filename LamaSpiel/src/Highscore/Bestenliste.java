b1ef04770c6be733debc27fc7817ef767361e44c
Vanessa Stein <steinv@rhrk.uni-kl.de>
2021-06-11 16:17:57 +0200

    Kleine Veränderungen an Bestenliste

--- LamaSpiel/src/Highscore/Bestenliste.java
+++ LamaSpiel/src/Highscore/Bestenliste.java
@@ -4,8 +4,6 @@ package Highscore;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import RMI.RMIClient;
-
 
 
 /**
@@ -13,25 +11,24 @@ import RMI.RMIClient;
  *
  * @author Vanessa Stein
  */
+
 public class Bestenliste {
 
-    public static HashMap<String,Integer> bestenliste = new HashMap<String,Integer>();
-
+    private HashMap<String,Integer> bestenliste = new HashMap<String,Integer>();
 
     /**
-     * Die Methode fügt einen neuen Eintrag zur Bestenliste hinzu oder aktualisiert einen bestehenden Eintrag
+     * Die Methode fügt einen neuen Eintrag zur Bestenliste hinzu oder aktualisiert den Score eines bestehenden Eintrags
      * @param benutzername Benutzername des Spielers, dessen Highscore eingetragen werden soll
      * @param isBot() überprüft, ob Spieler kein Bot ist
      */
-    public static void eintragHinzufuegen(String benutzername, boolean isBot){
-        if (!isBot){
-            if(bestenliste.containsKey(benutzername)){
-                bestenliste.replace(benutzername, bestenliste.get(benutzername)+1);
+    public void eintragHinzufuegen(String benutzername, boolean isBot) {
+        if (!isBot) {
+            if (bestenliste.containsKey(benutzername)) {
+                bestenliste.replace(benutzername, bestenliste.get(benutzername) + 1);
             } else {
-                bestenliste.put(benutzername,1);
+                bestenliste.put(benutzername, 1);
             }
         }
-
     }
 
 
@@ -39,15 +36,14 @@ public class Bestenliste {
      * Die Methode dient zum Abrufen des Highscores eines Spielers
      * @param benutzername Benutzername des Spielers, dessen Score abgerufen werden soll
      * @return gibt die Anzahl der gewonnenen Spiele als Integer zurück
-     * @throws NoSuchElementException Wirft Exeption, falls Eintrag nicht existiert
+     * @throws NullPointerException Wirft Exeption, falls Eintrag nicht existiert
      */
-    public static Integer getScore(String benutzername) throws NoSuchElementException {
+    public Integer getScore(String benutzername) throws NullPointerException {
         try {
             return (bestenliste.get(benutzername));
-        } catch (NoSuchElementException e){
-
+        } catch (NullPointerException e){
+            return null;
         }
-        return null;
     }
 
 
@@ -55,7 +51,7 @@ public class Bestenliste {
      * Die Methode dient zum Abrufen der zehn Spielernamen mit dem besten Score
      * @return gibt eine ArrayList mit den zehn besten Eintraegen zurueck
      */
-    public static ArrayList getTopZehn(){
+    public ArrayList getTopZehn(){
         Stream<Map.Entry<String,Integer>> sorted =
                 bestenliste.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()));
 
@@ -68,12 +64,12 @@ public class Bestenliste {
     /**
      * Die Methode entfernt einen Eintrag aus der Bestenliste, z.B. wenn das zugehoerige Konto geloescht wird
      * @param benutzername Benutzername des Spielers, dessen Eintrag entfernt werden soll
-     * @throws NoSuchElementException Wirft Exeption, falls Eintrag nicht vorhanden ist
+     * @throws NullPointerException Wirft Exeption, falls Eintrag nicht vorhanden ist
      */
-    public static void eintragLoeschen(String benutzername) throws NoSuchElementException{
+    public void eintragLoeschen(String benutzername) throws NullPointerException{
         try {
             bestenliste.remove(benutzername);
-        } catch (NoSuchElementException e){
+        } catch (NullPointerException e){
 
         }
 
