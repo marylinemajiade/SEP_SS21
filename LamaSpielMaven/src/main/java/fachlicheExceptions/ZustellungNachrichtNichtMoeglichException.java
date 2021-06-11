c5c41205e2f6917ccf79d22a62900c4a52abe7fd
Nick Jochum <njochum@rhrk.uni-kl.de>
2021-06-11 19:48:16 +0200

    Error behoben

--- LamaSpielMaven/src/main/java/fachlicheExceptions/ZustellungNachrichtNichtMoeglichException.java
+++
@@ -1,7 +0,0 @@
-package fachlicheExceptions;
-
-//wird geworfen, wenn die Nachricht nicht zugestellt werden konnte.
-
-public class ZustellungNachrichtNichtMoeglichException extends Exception{
-    public ZustellungNachrichtNichtMoeglichException (String errorMessage) {super(errorMessage); }
-}
--- LamaSpielMaven/src/main/java/fachlicheExceptions/spielLaeuftBereitsException.java
+++
@@ -1,8 +0,0 @@
-package fachlicheExceptions;
-
-//Wird geworfen, wenn ein Spiel zu starten versucht wird, welches bereits läuft
-public class spielLaeuftBereitsException extends Exception{
-    public spielLaeuftBereitsException (String errorMessage) {
-        super(errorMessage);
-    }
-}
